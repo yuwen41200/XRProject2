189dc43e3db2bf3ed677d0290a191ca7c56a4a23
Yu-Wen Pu <ywpu@cs.nctu.edu.tw>
2020-04-22 22:42:50 +0800

    Update

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+
+    [SerializeField] private float unitPerSecond = 25;
+
+    // Start is called before the first frame update
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        var eulers = new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+        eulers *= Time.deltaTime * unitPerSecond;
+        transform.Rotate(eulers, Space.Self);
+    }
+
+}
---
+++ Assets/SelectItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectItem : MonoBehaviour {
+
+    public float triggerDuration = 2;
+
+    public Vector3 photoDirection = new Vector3(0.8f, -0.6f, 0.3f);
+    public Image photoTriggerIcon;
+    private float _photoTriggerTimer;
+
+    public Vector3 headsetDirection = new Vector3(0.9f, -0.4f, -0.1f);
+    public Image headsetTriggerIcon;
+    private float _headsetTriggerTimer;
+
+    void Update() {
+        Vector3 cameraDirection = transform.forward;
+        if (Vector3.Dot(cameraDirection, headsetDirection) > 0.95) {
+            _headsetTriggerTimer += Time.deltaTime;
+            headsetTriggerIcon.fillAmount = _headsetTriggerTimer / triggerDuration;
+            if (_headsetTriggerTimer > triggerDuration) {
+                // triggered
+            }
+        }
+        else {
+            _headsetTriggerTimer = 0;
+            headsetTriggerIcon.fillAmount = _headsetTriggerTimer / triggerDuration;
+        }
+    }
+
+}
