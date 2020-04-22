a4c564dc94903dea5f2e45fb02e3090317d36a01
Yu-Wen Pu <ywpu@cs.nctu.edu.tw>
2020-04-23 02:03:00 +0800

    Update

--- Assets/SelectItem.cs
+++ Assets/SelectItem.cs
@@ -1,31 +1,61 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectItem : MonoBehaviour {
 
     public float triggerDuration = 2;
+    public float activeDuration = 10;
+    private bool _isTriggered;
 
     public Vector3 photoDirection = new Vector3(0.8f, -0.6f, 0.3f);
     public Image photoTriggerIcon;
     private float _photoTriggerTimer;
+    public GameObject photoTriggerImage;
 
     public Vector3 headsetDirection = new Vector3(0.9f, -0.4f, -0.1f);
     public Image headsetTriggerIcon;
     private float _headsetTriggerTimer;
+    public GameObject headsetTriggerImage;
 
-    void Update() {
+    private void Update() {
+
         Vector3 cameraDirection = transform.forward;
-        if (Vector3.Dot(cameraDirection, headsetDirection) > 0.95) {
+
+        if (!_isTriggered && Vector3.Dot(cameraDirection, headsetDirection) > 0.95) {
             _headsetTriggerTimer += Time.deltaTime;
             headsetTriggerIcon.fillAmount = _headsetTriggerTimer / triggerDuration;
             if (_headsetTriggerTimer > triggerDuration) {
-                // triggered
+                headsetTriggerImage.SetActive(true);
+                _isTriggered = true;
+                StartCoroutine(WaitAndDeactivate(headsetTriggerImage));
             }
         }
         else {
             _headsetTriggerTimer = 0;
-            headsetTriggerIcon.fillAmount = _headsetTriggerTimer / triggerDuration;
+            headsetTriggerIcon.fillAmount = 0;
         }
+
+        if (!_isTriggered && Vector3.Dot(cameraDirection, photoDirection) > 0.95) {
+            _photoTriggerTimer += Time.deltaTime;
+            photoTriggerIcon.fillAmount = _photoTriggerTimer / triggerDuration;
+            if (_photoTriggerTimer > triggerDuration) {
+                photoTriggerImage.SetActive(true);
+                _isTriggered = true;
+                StartCoroutine(WaitAndDeactivate(photoTriggerImage));
+            }
+        }
+        else {
+            _photoTriggerTimer = 0;
+            photoTriggerIcon.fillAmount = 0;
+        }
+
+    }
+
+    private IEnumerator WaitAndDeactivate(GameObject triggerImage) {
+        yield return new WaitForSeconds(activeDuration);
+        triggerImage.SetActive(false);
+        _isTriggered = false;
     }
 
 }
