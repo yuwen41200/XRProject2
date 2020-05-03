cd453a4c806c1976e726f64b96ec251f8cf55693
Yu-Wen Pu <ywpu@cs.nctu.edu.tw>
2020-05-03 08:04:51 +0800

    Update

--- Assets/SelectItem1.cs
+++ Assets/SelectItem1.cs
@@ -11,16 +11,22 @@ public class SelectItem1 : MonoBehaviour {
     public GameObject blackScreen;
     private Image _blackScreenImage;
 
+    public Vector3 headsetDirection = new Vector3(-0.6f, -0.7f, 0.5f);
+    public GameObject headsetCanvas;
+    public Image headsetTriggerIcon;
+    private float _headsetTriggerTimer;
+
     public Vector3 photoDirection = new Vector3(0.8f, -0.6f, 0.3f);
     public GameObject photoCanvas;
     public Image photoTriggerIcon;
     private float _photoTriggerTimer;
     public GameObject photoTriggerImage;
 
-    public Vector3 headsetDirection = new Vector3(-0.6f, -0.7f, 0.5f);
-    public GameObject headsetCanvas;
-    public Image headsetTriggerIcon;
-    private float _headsetTriggerTimer;
+    public Vector3 phoneDirection = new Vector3(-0.6f, -0.7f, 0.5f);
+    public GameObject phoneCanvas;
+    public Image phoneTriggerIcon;
+    private float _phoneTriggerTimer;
+    public GameObject phoneTriggerImage;
 
     private void Update() {
 
@@ -50,9 +56,22 @@ public class SelectItem1 : MonoBehaviour {
             photoTriggerIcon.fillAmount = 0;
         }
 
+        if (!_isTriggered && Vector3.Dot(cameraDirection, phoneDirection) > 0.97) {
+            _phoneTriggerTimer += Time.deltaTime;
+            phoneTriggerIcon.fillAmount = _phoneTriggerTimer / triggerDuration;
+            if (_phoneTriggerTimer > triggerDuration) {
+                StartCoroutine(TriggerItem(phoneTriggerImage));
+            }
+        }
+        else {
+            _phoneTriggerTimer = 0;
+            phoneTriggerIcon.fillAmount = 0;
+        }
+
     }
 
     private IEnumerator TriggerItem(GameObject triggerImage) {
+
         var tImage = triggerImage.GetComponent<Image>();
         var tAudio = triggerImage.GetComponent<AudioSource>();
         Color newColor;
@@ -83,6 +102,7 @@ public class SelectItem1 : MonoBehaviour {
         }
         triggerImage.SetActive(false);
         _isTriggered = false;
+
     }
 
     private IEnumerator FadeIn() {
@@ -93,15 +113,17 @@ public class SelectItem1 : MonoBehaviour {
             _blackScreenImage.color = newColor;
             yield return new WaitForSeconds(0.1f);
         }
-        photoCanvas.SetActive(true);
         headsetCanvas.SetActive(true);
+        photoCanvas.SetActive(true);
+        phoneCanvas.SetActive(true);
         _isTriggered = false;
     }
 
     private IEnumerator FadeOut() {
         _isTriggered = true;
-        if (photoCanvas) photoCanvas.SetActive(false);
         if (headsetCanvas) headsetCanvas.SetActive(false);
+        if (photoCanvas) photoCanvas.SetActive(false);
+        if (phoneCanvas) phoneCanvas.SetActive(false);
         for (var alpha = 0f; alpha <= 1; alpha += 0.025f) {
             var newColor = _blackScreenImage.color;
             newColor.a = alpha;
@@ -123,8 +145,9 @@ public class SelectItem1 : MonoBehaviour {
             resetColor.a = 0;
             _blackScreenImage.color = resetColor;
         }
-        _photoTriggerTimer = 0;
         _headsetTriggerTimer = 0;
+        _photoTriggerTimer = 0;
+        _phoneTriggerTimer = 0;
         _isTriggered = false;
         goToScene2 = false;
     }
